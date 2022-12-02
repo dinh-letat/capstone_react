1564f7a7b544c20ef8875e8e812e62d073fa553c
Le Tat Dinh <108498437+dinh-letat@users.noreply.github.com>
2022-12-03 02:23:56 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./Redux/configStore";
+import { Routes, Route, BrowserRouter,unstable_HistoryRouter as HistoryRouter, Navigate } from "react-router-dom";
+import Index from "./Pages/Index";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
+import Carts from "./Pages/Carts";
+import Search from "./Pages/Search";
+import Detail from "./Pages/Detail";
+import HomeTemplate from "./Templates/HomeTemplate";
+import { createBrowserHistory } from "history";
+import ResponsiveItem from "./Responsive/ResponsiveItem";
+import Mobile_Carts from "./Pages/Mobile/Mobile_Carts";
+export const history = createBrowserHistory();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+        <Routes>
+          <Route path="" element={<HomeTemplate />}>
+            <Route index element={<Index/>}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="profile" element={<Profile />}></Route>
+            <Route path="carts" element={<ResponsiveItem component={Carts} mobileComponent={Mobile_Carts} />}></Route>
+            <Route path="search" element={<Search />}></Route>
+            <Route path="detail" >
+              <Route path=":id" element={<Detail />}></Route>
+            </Route> 
+            <Route path="*" element={<Navigate to='' />}></Route>            
+          </Route>
+        </Routes>
+    </HistoryRouter>
+  </Provider>
 );
